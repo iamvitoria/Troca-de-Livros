19d75f6c8ef09baba95e97bb4f170275f8dd510d
Renan Bordignon Poy <renanbp2@gmail.com>
2024-06-01 10:25:46 -0300

    ajuste sidebar

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('perfil/', PerfilView.as_view(), name='perfil'),
+    path('conversas/', ConversasView.as_view(), name='conversas'),
+    path('meusitens/', MeusItensView.as_view(), name='meusitens'),
 ]
